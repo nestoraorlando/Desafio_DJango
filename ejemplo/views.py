5cbf0f06e4b2b762610e269221339553e5a9c372
Nestor <nestoraorlando@gmail.com>
2022-10-17 01:39:25 -0300

    Segundo commit luego de agregar las funciones de agregar nombres y apellidos, crear bucles for, condicionales if y agregar fn IMC

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -2,4 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "ejemplo/saludar.html")
+    return render(request, "ejemplo/saludar.html", {"nombre":"Néstor","apellido":"Orlando"})
+
+def index_dos(request, nombre, apellido):
+    return render(request, "ejemplo/saludar.html", {"nombre":nombre,"apellido":apellido})
+
+def index_tres(request):
+
+    return render(request, "ejemplo/saludar.html",
+    {"notas":[1,2,3,4,5,6,7,8,9]}
+    )
+
+
+def i_m_corporal(request, peso, altura):
+    imc = int(peso) / (int(altura)/100) ** (int(altura)/100)
+    return render(request, "ejemplo/imc.html", {"imc":str(imc)})
